c06ef98e0adf7e0e4e6e0b9e290cb4e3b2f1014c
sym-monster <727455438@qq.com>
2023-11-20 12:48:29 +0800

    Improved MusicDetail UI detail

--- src/components/MusicDetail.vue
+++ src/components/MusicDetail.vue
@@ -16,6 +16,11 @@
             </div>
             <van-icon name="share-o" class="iconShare" size=".5rem" />
         </div>
+        <div class="disc">
+            <img src="@/assets/disc-plus.png" alt="disc-plus" class="discPlus">
+            <img src="@/assets/needle-ab.png" alt="needle-ab" class="needleAB">
+            <img :src="musicInfo.al.picUrl" alt="music-cover" class="mucicCover">
+        </div>
     </div>
 </template>
 
@@ -33,7 +38,7 @@ export default {
     width: 100%;
     height: 100%;
     position: absolute;
-    z-index: -1;
+    z-index: -3;
     filter: blur(30px);
 }
 
@@ -87,6 +92,31 @@ export default {
         color: white;
         padding-right: .2rem;
         padding-left: .2rem;
+    }
+}
+.disc {
+    width: 100%;
+    height:50vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .discPlus {
+        width: 4rem;
+        height: 4rem;
+        z-index: -2;
+    }
+    .needleAB {
+        height: 3.2rem;
+        position: absolute;
+        top: 10%;
+        right: 25%;
+    }
+    .mucicCover {
+        width: 2.6rem;
+        height: 2.6rem;
+        position: absolute;
+        border-radius: 50%;
+        z-index: -1;
     }
 }
 </style>
